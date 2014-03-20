5b1738f4377ac32be3b32faccc2c43d03c570d56
Ankur Singh <ankur13019@iiitd.ac.in>
2014-03-20 15:14:56 +0530

    heroku mailer bug try v2

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 UserAuth::Application.initialize!
-YourApplicationName::Application.configure do
+UserAuth::Application.configure do
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
