f61480e12c4c3b243ded78468600004dd3d4d71e
Ankur Singh <ankur13019@iiitd.ac.in>
2014-03-20 15:26:27 +0530

    heroku mailer change port to 25

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ UserAuth::Application.configure do
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
-  port:                 587,
+  port:                 25,
   domain:               'gmail.com',
   user_name:            'ror1github@gmail.com',
   password:             'devotestror1',
