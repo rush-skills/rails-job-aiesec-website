b37cc3a77fc2c3bdc02209017eff539f1cf2d50c
Ankur Singh <ankur13019@iiitd.ac.in>
2014-03-21 02:27:16 +0530

    mailer fix adding heroku's gmail mailers

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 UserAuth::Application.initialize!
-UserAuth::Application.configure do
-	config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  user_name:            'ror1github@gmail.com',
-  password:             'devotestror1',
-  authentication:       'plain',
-  enable_starttls_auto: true  }
- end
