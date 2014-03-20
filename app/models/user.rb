76778b566caef515c27d1860b7dcd212436f7cd3
Ankur Singh <ankur13019@iiitd.ac.in>
2014-03-20 12:34:10 +0530

    commit after setting up to use postgres db and before starting mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   #Only on Create so other actions like update password attribute can be nil
   validates_length_of :password, :in => 6..20, :on => :create
 
-  attr_accessible :username, :email, :password, :password_confirmation, :app_for
+  attr_accessible :username, :email, :password, :password_confirmation, :app_for, :is_admin
   
 
   def self.authenticate(username_or_email="", login_password="")
